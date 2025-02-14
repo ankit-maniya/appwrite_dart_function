b08441c47e2308cd31760fbadeaa21e0d9fc265b
Ankit Maniya <ankit.maniya6588@gmail.com>
2025-02-14 09:51:47 -0700

    changed folder structure

--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-
-import 'package:dart_appwrite/dart_appwrite.dart';
-
-void main(List<String> args) {
-  Map<String, String> envVars = Platform.environment;
-  print(envVars['APPWRITE_PROJECT_ID']);
-  print('Hello world!');
-}
